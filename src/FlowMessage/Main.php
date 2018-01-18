d620f21f58e59b0de8d82185c97f9c4db90703b6
DevrlyCode <aaronwilliams2019@hotmail.com>
2018-01-17 20:57:00 -0600

    Update Main.php

--- src/FlowMessage/Main.php
+++ src/FlowMessage/Main.php
@@ -14,15 +14,10 @@ class Main extends PluginBase implements Listener{
     $this->getLogger()->notice("FlowMessage Plugin Activated");
   }
 
-	public function onDisable(){
+  public function onDisable(){
 		$this->getServer()->getLogger()->info("FlowMessage Plugin Activated");
   }
-  
-  public funtion onJoin(PlayerJoinEvent $event){
-    $player = $event->getPlayer();
-    
-    $this->sendMessage(TF::RED.">". TF::GRAY. $player. TF::WHITE." Joined The Game!");
-  }
+
  //This sends a message to that player directly when they connect
   public function onPlayerJoin(PlayerJoinEvent $event){
      $p = $event->getPlayer();
