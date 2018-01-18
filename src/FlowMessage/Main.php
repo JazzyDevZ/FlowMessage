24f9b2f37b8179f62779006dbba7c0782aef813b
DevrlyCode <aaronwilliams2019@hotmail.com>
2018-01-17 21:42:37 -0600

    2.0

--- src/FlowMessage/Main.php
+++ src/FlowMessage/Main.php
@@ -1,34 +1,21 @@
 <?php
 
 namespace FlowMessage;
-
-use pocketmine\plugin\PluginBase as Base;
-use poocketmine\Server;
-use pocketmine\Player;
-use pocketmine\event\Event;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\Config;
+ 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
-
-class Main extends Base implements Listener{
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+      
+class Main extends PluginBase implements Listener{
+     public function onEnable(){
+     $this->getServer()->getPluginManager->registerEvents($this);
+     $this->getLogger()->info("FlowMessage enabled!");
+     }
 	
-	    public $cfg;
-	    public function onLoad(){
-	         $this->getLogger()->info("FlowMessage Plugin Activating...");
-	    }
-	
-	    public function onEnable(){
-	         $this->saveDefaultConfig();
-	         $this->reloadConfig();
-	         $this->getLogger()->info("FlowMessage Plugin Activated");
-	         $this->getServer()->getPluginManager()->registerEvents($this,$this);
-	  }
-	
-	  public function onJoin(PlayerJoinEvent $event){
-	        $player = $event->getPlayer();
-	        $msg = $this->getConfig()->get("message");
-	        $msg = str_replace("{player}", $player->getName(), $msg);
-	        $player->sendMessage($msg);   
-		}
-  }
+     public function onPlayerJoin(PlayerJoinEvent $event){
+     $p = $event->getPlayer();
+     $p->sendMessage("Welcome to my server!");
+     }
+}
