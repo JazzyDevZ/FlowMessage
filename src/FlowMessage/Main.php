1bbe04448e8244aeabb0344c75f8ac4ff1055437
DevrlyCode <aaronwilliams2019@hotmail.com>
2018-01-17 21:06:40 -0600

    Update Main.php

--- src/FlowMessage/Main.php
+++ src/FlowMessage/Main.php
@@ -2,25 +2,23 @@
 
 namespace FlowMessage;
 
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
-use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat as TF;
 
 class Main extends PluginBase implements Listener{
-
-  public function onEnable(){
-    $this->getLogger()->notice("FlowMessage Plugin Activated");
-  }
-
-  public function onDisable(){
-		$this->getServer()->getLogger()->info("FlowMessage Plugin Activated");
-  }
-
- //This sends a message to that player directly when they connect
-  public function onPlayerJoin(PlayerJoinEvent $event){
-     $p = $event->getPlayer();
-     $p->sendMessage(TextFormat::GRAY."Welcome, you are using FlowMessage!"."\n". TextFormat::RED."Created by YungFlowz and DevrlyCode!");
-  }
-}
+	
+	public function onEnable(){
+		$this->getserver()->getLogger()->info("FlowMessage Plugin Activated")
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onDisable(){
+		$this->getserver()->getLogger()->info("FlowMessage Plugin Deactivated")
+	}
+	
+	public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPLayer();
+		$player->sendMessage(TF::RED . ">" . TF::GRAY . "Welcome, you are using FlowMessage \n Made by DevrlyCode and YungFlowz!";
+		{
